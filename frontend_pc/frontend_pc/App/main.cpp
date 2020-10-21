3ffcf5ae8e6391ff580a4325d2057946b67fcb31
sukamay <253246887@qq.com>
2020-10-21 22:42:06 +0800

    feat(frontend_pc):update structure of frontend and add test case

--- frontend_pc/frontend_pc/App/main.cpp
+++ frontend_pc/frontend_pc/App/main.cpp
@@ -10,10 +10,11 @@ int main(int argc, char *argv[])
     MainWindow w;
     LoginForm login_form;
 
-    QFile qss(":/login.qss");
-    qss.open(QFile::ReadOnly);
-    login_form.setStyleSheet(qss.readAll());
-    qss.close();
+//    TODO: consider style after all functions have done
+//    QFile qss(":/login.qss");
+//    qss.open(QFile::ReadOnly);
+//    login_form.setStyleSheet(qss.readAll());
+//    qss.close();
 
     int ret = login_form.exec();
     if (ret == QDialog::Accepted) {
---
+++ frontend_pc/frontend_pc/frontend_pc.pro
@@ -0,0 +1,6 @@
+TEMPLATE = subdirs
+#定义了ordered表示子项目按照添加的顺序来编译
+CONFIG  += ordered
+SUBDIRS += \
+    App \
+    Test
