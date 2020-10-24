c1c3afd83258ef4ad491407f4a8453b444abdfdc
leevius <leevius@umich.edu>
2020-10-24 16:32:51 +0800

    feat(qt): initialize http communication class

--- frontend_qt/communhttp.cpp
+++ frontend_qt/communhttp.cpp
@@ -1,6 +1,17 @@
 #include "communhttp.h"
 
-communhttp::communhttp()
+communhttp::communhttp(QObject *parent) : QObject (parent)
 {
+    manager = new QNetworkAccessManager();
+    connect(manager, &QNetworkAccessManager::finished,
+            this, &communhttp::replyfinished);
+}
 
+void communhttp::replyfinished(QNetworkReply *reply){
+    if (reply->error()){
+        qDebug() << reply->errorString();
+        return;
+    }
+    QString answer = reply->readAll();
+    qDebug() << answer;
 }
--- frontend_qt/communhttp.h
+++ frontend_qt/communhttp.h
@@ -1,11 +1,25 @@
 #ifndef COMMUNHTTP_H
 #define COMMUNHTTP_H
 
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
 
-class communhttp
-{
+class communhttp : public QObject{
+    Q_OBJECT
+private:
+    // the network manager handling the communication
+    QNetworkAccessManager * manager;
+    // the request sent to server: GET, POST, etc.
+    QNetworkRequest request;
+
+private slots:
+    // after reply is finished -- do the following
+    void replyfinished(QNetworkReply *reply);
+
 public:
-    communhttp();
+    explicit communhttp(QObject *parent = nullptr);
 };
 
 #endif // COMMUNHTTP_H
