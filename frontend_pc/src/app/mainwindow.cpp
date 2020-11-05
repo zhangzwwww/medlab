3433e0f558cfa28739db6dd25ea8bea7d93423f1
YUYUN-ZHAO <im.yuyunzhao@gmail.com>
2020-11-05 20:17:13 +0800

    bug: fix the bug

--- frontend_pc/src/app/mainwindow.cpp
+++ frontend_pc/src/app/mainwindow.cpp
@@ -7,6 +7,9 @@
 #include "vtkAutoInit.h"
 VTK_MODULE_INIT(vtkRenderingOpenGL2);
 VTK_MODULE_INIT(vtkInteractionStyle);
+VTK_MODULE_INIT(vtkRenderingVolumeOpenGL2);
+
+
 #include "vtkSmartPointer.h"
 #include "vtkDICOMImageReader.h"
 #include "vtkImageViewer2.h"
