a50033b99d1f99268db4f45ba107f6f4589d5217
lee-vius <leevius@umich.edu>
2021-01-06 02:14:19 +0800

    fix: download bug fixed

--- frontend_pc/src/app/commu/imageinfo.cpp
+++ frontend_pc/src/app/commu/imageinfo.cpp
@@ -122,6 +122,7 @@ void imageInfo::downloadFile(QNetworkReply* reply){
         QFile meta(dir.filePath("meta_data"));
         if (!meta.open(QIODevice::Append)){
             qDebug() << "Open meta file fails";
+            reply->deleteLater();
             return;
         }
         // normal case
@@ -152,6 +153,7 @@ void imageInfo::downloadFile(QNetworkReply* reply){
                 meta.write("\r\n");
             }
         }
+        meta.close();
     }
     reply->deleteLater();
 }
@@ -322,13 +324,14 @@ int imageInfo::uploadImgMark(QString folderpath, int level, int view, double top
     json_doc.setObject(json_content);
     QByteArray data = json_doc.toJson(QJsonDocument::Compact);
     // construct request
+    QNetworkAccessManager local_qnam;
     QNetworkRequest request;
     QUrl url(urlbase["base2"] + urlbase["mark"]);
     request.setUrl(url);
     request.setRawHeader("X-Auth-Token", token.toUtf8());
     request.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("application/json"));
     // send request
-    QNetworkReply* reply = qnam.post(request, data);
+    QNetworkReply* reply = local_qnam.post(request, data);
     // wait until server reply
     QEventLoop eventloop;
     connect(reply, SIGNAL(finished()), &eventloop, SLOT(quit()));
@@ -381,11 +384,12 @@ QVector<imageInfo::imgMark> imageInfo::getAllMarks(QString folderpath, int layer
     url.setQuery(query.query());
 
     // construct the request
+    QNetworkAccessManager local_qnam;
     QNetworkRequest request;
     request.setUrl(url);
     request.setRawHeader("X-Auth-Token", token.toUtf8());
     // send request
-    QNetworkReply* reply = qnam.get(request);
+    QNetworkReply* reply = local_qnam.get(request);
     // loop until get reply
     QEventLoop eventloop;
     connect(reply, SIGNAL(finished()), &eventloop, SLOT(quit()));
@@ -482,6 +486,7 @@ QString imageInfo::predictImageHttp(QString filepath, QString patientID){
 
 QVector<QString> imageInfo::getCtimeHttp(QString patientId){
     // construct url and query item
+    QNetworkAccessManager local_qnam;
     QUrl url(urlbase["base2"] + urlbase["image"] + "/ctime");
     QUrlQuery query;
     query.addQueryItem("patientId", patientId);
@@ -491,7 +496,7 @@ QVector<QString> imageInfo::getCtimeHttp(QString patientId){
     request.setUrl(url);
     request.setRawHeader("X-Auth-Token", token.toUtf8());
     // send request
-    QNetworkReply* reply = qnam.get(request);
+    QNetworkReply* reply = local_qnam.get(request);
 
     QEventLoop eventloop;
     connect(reply, SIGNAL(finished()), &eventloop, SLOT(quit()));
@@ -514,6 +519,7 @@ QVector<QString> imageInfo::getCtimeHttp(QString patientId){
     if (status == 200){
         // normal case
         QByteArray resp = reply->readAll();
+        // qDebug() << resp;
         QJsonParseError jerror;
         QJsonDocument json = QJsonDocument::fromJson(resp, &jerror);
         if (jerror.error == QJsonParseError::NoError && !json.isNull() && !json.isEmpty()){
